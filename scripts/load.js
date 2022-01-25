4492b3bca0c116ed870647ed1e8f7cc3ea63a5c0
wayofDao <giedriusthedev@gmail.com>
2022-01-25 10:48:50 +0100

    adds load screen structure and basic logic

---
+++ scripts/load.js
@@ -0,0 +1,25 @@
+let storedLists = {};
+
+
+
+function displayLists() {
+    const listTarget = document.querySelector('#shoppingLists')
+
+    Object.keys(storedLists).forEach(key => {
+        let div = document.createElement('div');
+        div.classList.add('load-list');
+        let a = document.createElement('a');
+        a.setAttribute('href', `../list.html?list-name=${key}`)
+        a.textContent = key
+        div.appendChild(a);
+        listTarget.appendChild(div);
+    })
+}
+
+function initLoadScreen() {
+    storedLists = JSON.parse(localStorage.getItem('myLists'));
+    displayLists();
+}
+
+
+window.onload = initLoadScreen;
