818e8f8ce365dab11c5ad163ddbee3d57a8ef596
wayofDao <giedriusthedev@gmail.com>
2022-01-26 09:28:23 +0100

    adds basic shopping list view and logic

--- scripts/load.js
+++ scripts/load.js
@@ -9,7 +9,7 @@ function displayLists() {
         let div = document.createElement('div');
         div.classList.add('load-list');
         let a = document.createElement('a');
-        a.setAttribute('href', `../list.html?list-name=${key}`)
+        a.setAttribute('href', `../pages/list.html?list-name=${key}`)
         a.textContent = key
         div.appendChild(a);
         listTarget.appendChild(div);
